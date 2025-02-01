50478f0c304665de907557a18471b35d3e22b5b7
Sonny <sonnybanzon090997@gmail.com>
2025-02-01 19:04:36 +0800

    Setting up login

--- auth/login.php
+++ auth/login.php
@@ -1,4 +1,13 @@
 
+<?php
+// Start the session
+session_start();
+
+// Generate a CSRF token if it doesn't exist
+if (empty($_SESSION['csrf_token'])) {
+  $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+}
+?>
 
 
 
@@ -29,345 +38,15 @@
   <div class="page-wrapper auth-bg">
     <div class="main-wrapper bg-gradient-black">
       <div class="global-styles w-embed">
-        <style>
-/* Make text look crisper and more legible in all browsers */
-body {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  font-smoothing: antialiased;
-  text-rendering: optimizeLegibility;
-}
-/* Focus state style for keyboard navigation for the focusable elements */
-*[tabindex]:focus-visible,
-input[type="file"]:focus-visible {
- outline: 0.125rem solid #4d65ff;
- outline-offset: 0.125rem;
-}
-/* Get rid of top margin on first element in any rich text element */
-.w-richtext > :not(div):first-child, .w-richtext > div:first-child > :first-child {
-  margin-top: 0 !important;
-}
-/* Get rid of bottom margin on last element in any rich text element */
-.w-richtext>:last-child, .w-richtext ol li:last-child, .w-richtext ul li:last-child {
-    margin-bottom: 0 !important;
-}
-/* Prevent all click and hover interaction with an element */
-.pointer-events-off {
-    pointer-events: none;
-}
-/* Enables all click and hover interaction with an element */
-.pointer-events-on {
-  pointer-events: auto;
-}
-/* Create a class of .div-square which maintains a 1:1 dimension of a div */
-.div-square::after {
-    content: "";
-    display: block;
-    padding-bottom: 100%;
-}
-/* Make sure containers never lose their center alignment */
-.container-medium,.container-small, .container-large {
-    margin-right: auto !important;
-    margin-left: auto !important;
-}
-/* 
-Make the following elements inherit typography styles from the parent and not have hardcoded values. 
-Important: You will not be able to style for example "All Links" in Designer with this CSS applied.
-Uncomment this CSS to use it in the project. Leave this message for future hand-off.
-*/
-/*
-a,
-.w-input,
-.w-select,
-.w-tab-link,
-.w-nav-link,
-.w-dropdown-btn,
-.w-dropdown-toggle,
-.w-dropdown-link {
-  color: inherit;
-  text-decoration: inherit;
-  font-size: inherit;
-}
-*/
-/* Apply "..." after 3 lines of text */
-.text-style-3lines {
-    display: -webkit-box;
-    overflow: hidden;
-    -webkit-line-clamp: 3;
-    -webkit-box-orient: vertical;
-}
-/* Apply "..." after 2 lines of text */
-.text-style-2lines {
-    display: -webkit-box;
-    overflow: hidden;
-    -webkit-line-clamp: 2;
-    -webkit-box-orient: vertical;
-}
-/* Adds inline flex display */
-.display-inlineflex {
-  display: inline-flex;
-}
-/* These classes are never overwritten */
-.hide {
-  display: none !important;
-}
-.margin-0 {
-  margin: 0rem !important;
-}
-.padding-0 {
-  padding: 0rem !important;
-}
-.spacing-clean {
-    padding: 0rem !important;
-    margin: 0rem !important;
-}
-.margin-top {
-  margin-right: 0rem !important;
-  margin-bottom: 0rem !important;
-  margin-left: 0rem !important;
-}
-.padding-top {
-  padding-right: 0rem !important;
-  padding-bottom: 0rem !important;
-  padding-left: 0rem !important;
-}
-.margin-right {
-  margin-top: 0rem !important;
-  margin-bottom: 0rem !important;
-  margin-left: 0rem !important;
-}
-.padding-right {
-  padding-top: 0rem !important;
-  padding-bottom: 0rem !important;
-  padding-left: 0rem !important;
-}
-.margin-bottom {
-  margin-top: 0rem !important;
-  margin-right: 0rem !important;
-  margin-left: 0rem !important;
-}
-.padding-bottom {
-  padding-top: 0rem !important;
-  padding-right: 0rem !important;
-  padding-left: 0rem !important;
-}
-.margin-left {
-  margin-top: 0rem !important;
-  margin-right: 0rem !important;
-  margin-bottom: 0rem !important;
-}
-.padding-left {
-  padding-top: 0rem !important;
-  padding-right: 0rem !important;
-  padding-bottom: 0rem !important;
-}
-.margin-horizontal {
-  margin-top: 0rem !important;
-  margin-bottom: 0rem !important;
-}
-.padding-horizontal {
-  padding-top: 0rem !important;
-  padding-bottom: 0rem !important;
-}
-.margin-vertical {
-  margin-right: 0rem !important;
-  margin-left: 0rem !important;
-}
-.padding-vertical {
-  padding-right: 0rem !important;
-  padding-left: 0rem !important;
-}
-.why-list.owl-carousel .owl-stage-outer
-{
-    overflow: visible;
-}
-.why-list.owl-carousel .owl-stage
-{
-    padding-left: 0;
-}
-.partners-list .owl-item img
-{
-    width: auto !important;
-}
-.partners-list.owl-carousel .owl-stage-outer, .testimonial-list.owl-carousel .owl-stage-outer, .about-gallery-list.owl-carousel .owl-stage-outer
-{
-    overflow: visible !important;
-}
-a
-{
-    color: #0A4DF6;
-}
-.select, .select-field {
-    appearance: none; /* Removes default arrow icon in some browsers */
-    -webkit-appearance: none; /* Removes default arrow icon in Safari */
-    -moz-appearance: none; /* Removes default arrow icon in Firefox */
-}
-.w-slide [aria-hidden="true"] {
-   height: 0px !important;
-}
-.dots-container .w-slider-dot 
-{
-  width: 25%;
-  height: 2px;
-  background-color: #717171;
-  border-radius: 0px !important;
-  padding:0 !important;
-  margin: 0 !important;
-}
-.dots-container .w-slider-dot.w-active
-{
-  background-color: #0629A3;
-}
-.dots-container .w-slider-dot:hover
-{
-  background-color: #0629A3;
-}
-.sidenav-button:hover .icon
-{
-    color: white;
-}
-.sidenav-button.w--current .icon
-{
-    color: white;
-}
-.recent-list .owl-stage
-{
-    padding-left: 0 !important;
-}
-.label span
-{
-    color: #EB5757;
-}   
-.active-swiper {
-  /* Make sure the container has enough height and width */
-  height: 100%;
-  width: 100%;
-}
-.swiper-slide {
-  /* Adjust styles for each slide as needed */
-  width: auto; /* or a specific width */
-}
-.tabs span
-{
-    font-size: 12px;
-    color: #D0222D;
-}
-/*For File Upload*/
-.file-upload {
-    display: flex;
-    align-items: center;
-    min-height: 34px;
-    color: #fff;
-}
-.file-upload.disabled
-{   
-    color: #828282;
-}
-.file-upload input[type="file"] {
-            display: none; /* Hide the default file input */
-}
-.file-upload label {
-    background-color: #5028FF;
-    padding: 8px 24px;
-    cursor: pointer;
-    border-radius: 50px;
-    margin: 0;
-}
-.file-upload.disabled label 
-{
-    background-color: #CACACA;
-}
-.file-name {
-    margin: 0 8px;
-    font-size: 14px;
-    font-weight: 400;
-    color: #171717;
-}
-.remove-file {
-    cursor: pointer;
-    background-image: url('https://uploads-ssl.webflow.com/665f147b743ba95cae446cfe/66a51772340eb291308ac0da_close_24px.svg');
-    background-size: contain;
-    background-repeat: no-repeat;
-    background-position: center;
-    width: 24px;
-    height: 24px;
-    display: none;
-}
-.scolling-wrapper::-webkit-scrollbar {
-  width: 0px;
-}
-.color
-{
-	margin: 0 14px;
-  width: 50px;
-  height: 50px;
-  padding: 1px 0px;
-  border: 1px solid #BDBDBD;
-  border-radius: 3px;
-  padding-block: 0px;
-  padding-inline: 0px;
-}
-.color-text-field
-{
- border: solid 1px #BDBDBD;
-}
-.label-text
-{
-	min-width: 100px;
-}
-.color
-{
-	flex: none;
-  cursor: pointer;
-}
-.radio-button-field .layout-radio.w--redirected-checked ~ .radio-phone-template {
-    border: solid 3px #19A733;
-}
-.radio-button-field .layout-radio.w--redirected-checked + .radio-phone-template {
-    border: solid 3px #19A733;
-}
-.radio-button-field.w--redirected-checked .radio-phone-template {
-    border: solid 3px #19A733;
-}
-@media screen and (max-width: 991px) {
-    .hide, .hide-tablet {
-        display: none !important;
-    }
-}
-@media screen and (max-width: 767px) {
-    .hide-mobile-landscape{
-      display: none !important;
-  }
-}
-@media screen and (max-width: 479px) {
-    .hide-mobile{
-      display: none !important;
-  }
-  .label-text
-  {
-    min-width: 95px;
-    font-size: 12px;
-  }
-  .color
-  {
-  	margin: 0 4px !important;
-  }
-  .color-field
-  {
-  	max-width: 55%;
-  }
-}
-/* Remove the spinner arrows for Chrome, Safari, Edge */
-input[type="number"]::-webkit-inner-spin-button,
-input[type="number"]::-webkit-outer-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
-}
-</style>
+
+
+        <!-- Global CSS -->
+        <?php include '../partials/global-css.css'; ?>
+
       </div>
 
-  <!-- Navigation -->
-  <?php include '../partials/navigation-dark.html'; ?>
+      <!-- Navigation -->
+      <?php include '../partials/navigation-dark.html'; ?>
 
 
       <section id="contact-form" class="section-auth">
@@ -375,67 +54,149 @@ input[type="number"]::-webkit-outer-spin-button {
           <div class="container-large">
             <div class="auth-wrapper">
               <div id="w-node-_6781a2ad-69e0-0135-a3da-b0405a444611-ae446d51" class="auth-form-block w-form">
-                <form id="wf-form-Login-Form" name="wf-form-Login-Form" data-name="Login Form" method="get" data-wf-page-id="665f147b743ba95cae446d51" data-wf-element-id="6781a2ad-69e0-0135-a3da-b0405a444612">
+                <form id="wf-form-Login-Form" name="wf-form-Login-Form" data-name="Login Form" action="../../private/auth/login-code.php" method="post" data-wf-page-id="665f147b743ba95cae446d51" data-wf-element-id="6781a2ad-69e0-0135-a3da-b0405a444612">
                   <h1 class="heading-style-h5">Welcome Back 👋</h1>
                   <div class="form-excerpt">Please login using the account details bellow.</div>
                   <div class="login-grid">
-                    <div class="field-wrapper"><input class="text-field transparent-text-field w-input" maxlength="256" name="Email" data-name="Email" placeholder="Email" type="email" id="Email" required=""></div>
-                    <div class="field-wrapper icon-field-wrapper"><input class="text-field transparent-text-field w-input" maxlength="256" name="Password" data-name="Password" placeholder="Password" type="password" id="Password" required=""><img src=".../../images/eye-slash.svg" loading="lazy" alt="" class="field-icon"></div>
+
+                    <div class="field-wrapper">
+
+                      <input type="hidden" name="csrf_token" class="text-field transparent-text-field w-input" value="<?php echo $_SESSION['csrf_token']; ?>">
+                    </div>
+
+
+                    <div class="field-wrapper"><input class="text-field transparent-text-field w-input" maxlength="256" name="email" data-name="Email" placeholder="Email" type="email" id="Email" required=""></div>
+
+
+                    <div class="field-wrapper icon-field-wrapper">
+                      <input class="text-field transparent-text-field w-input password-field" maxlength="256" name="password" id="Password" placeholder="Password" type="password" required>
+
+
+
+                      <img src=".../../images/eye-slash.svg" loading="lazy" alt="Toggle Password Visibility" class="field-icon toggle-icon" id="togglePassword">
+                    </div>
+
+
                     <div id="w-node-de69ede3-213b-09f7-1072-4a12ac329d2d-ae446d51" class="remember-wrapper"><label class="w-checkbox checkbox-field">
-                        <div class="w-checkbox-input w-checkbox-input--inputType-custom checkbox"></div><input type="checkbox" id="checkbox-2" name="checkbox-2" data-name="Checkbox 2" style="opacity:0;position:absolute;z-index:-1"><span class="cart-check-label w-form-label" for="checkbox-2">Remember Me</span>
-                      </label>
-                      <a href=".../../auth/forgot-password.php" class="purple-span">Forgot Password?</a>
-                    </div>
-                    <a data-w-id="6781a2ad-69e0-0135-a3da-b0405a444638" href="#" class="button is-icon max-button-width w-inline-block">
-                      <div class="btn-text">Login</div>
-                      <div class="icon-1x1-small w-embed"><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewbox="0 0 12 12" fill="none">
-                          <path fill-rule="evenodd" clip-rule="evenodd" d="M5.32481 1.89973C5.86086 2.62911 7.23527 3.75138 9.13918 3.69086L1.16196 8.29651L1.44768 8.79138L9.42395 4.18628C8.42036 5.80457 8.70502 7.5554 9.06858 8.38414L9.59187 8.15458C9.19323 7.24586 8.893 4.99296 10.9407 3.33777L10.7588 3.11272L10.7351 3.07171L10.6311 2.80164C8.17386 3.74738 6.37291 2.36093 5.78526 1.56133L5.32481 1.89973Z" fill="white"></path>
-                        </svg></div>
-                      <div class="button-overlay pointer-events-off"></div><input type="submit" data-wait="" class="submit-btn w-button" value="">
-                    </a>
+                      <div class="w-checkbox-input w-checkbox-input--inputType-custom checkbox"></div><input type="checkbox" id="checkbox-1" name="checkbox-2" data-name="Checkbox 2" style="opacity:0;position:absolute;z-index:-1"><span class="cart-check-label w-form-label" for="checkbox-2">Remember Me</span>
+                    </label>
+                    <a href=".../../auth/forgot-password.php" class="purple-span">Forgot Password?</a>
                   </div>
-                  <div class="google-wrapper">
-                    <div class="sign-with-wrapper">
-                      <div class="padd-12">OR SIGN IN WITH</div>
-                      <div class="line"></div>
-                    </div>
-                    <a href="#" class="button is-secondary icon-btn min-208 w-inline-block"><img src=".../../images/Google__G__Logo-1.svg" loading="lazy" alt="">
-                      <div>Google</div>
-                    </a>
-                    <div class="register-wrapper">
-                      <div class="dont-text">Don’t have an account yet?</div>
-                      <a href=".../../auth/register.php" class="underline-link">REGISTER NOW</a>
-                    </div>
+                  <a data-w-id="6781a2ad-69e0-0135-a3da-b0405a444638" href="#" class="button is-icon max-button-width w-inline-block">
+                    <div class="btn-text">Login</div>
+                    <div class="icon-1x1-small w-embed"><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewbox="0 0 12 12" fill="none">
+                      <path fill-rule="evenodd" clip-rule="evenodd" d="M5.32481 1.89973C5.86086 2.62911 7.23527 3.75138 9.13918 3.69086L1.16196 8.29651L1.44768 8.79138L9.42395 4.18628C8.42036 5.80457 8.70502 7.5554 9.06858 8.38414L9.59187 8.15458C9.19323 7.24586 8.893 4.99296 10.9407 3.33777L10.7588 3.11272L10.7351 3.07171L10.6311 2.80164C8.17386 3.74738 6.37291 2.36093 5.78526 1.56133L5.32481 1.89973Z" fill="white"></path>
+                    </svg></div>
+                    <div class="button-overlay pointer-events-off"></div><input type="submit" data-wait="" class="submit-btn w-button" value="">
+                  </a>
+                </div>
+                <div class="google-wrapper">
+                  <div class="sign-with-wrapper">
+                    <div class="padd-12">OR SIGN IN WITH</div>
+                    <div class="line"></div>
                   </div>
-                </form>
-                <div class="w-form-done">
-                  <div>Thank you! Your submission has been received!</div>
+                  <a href="#" class="button is-secondary icon-btn min-208 w-inline-block"><img src=".../../images/Google__G__Logo-1.svg" loading="lazy" alt="">
+                    <div>Google</div>
+                  </a>
+                  <div class="register-wrapper">
+                    <div class="dont-text">Don’t have an account yet?</div>
+                    <a href=".../../auth/register.php" class="underline-link">REGISTER NOW</a>
+                  </div>
                 </div>
-                <div class="w-form-fail">
-                  <div>Oops! Something went wrong while submitting the form.</div>
-                </div>
+              </form>
+              <div class="w-form-done">
+                <div>Thank you! Your submission has been received!</div>
+              </div>
+              <div class="w-form-fail">
+                <div>Oops! Something went wrong while submitting the form.</div>
               </div>
             </div>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
+
+
+    <!-- Footer section -->
+    <?php include '../partials/footer-dark.html'; ?>
+
+  </div>
+</div>
+<script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=665f147b743ba95cae446cfe" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script src=".../../js/webflow.js" type="text/javascript"></script>
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<script>
+// Get the current year
+  var currentYear = new Date().getFullYear();
+// Get the current year when the page loads
+  $(document).ready(function() {
+    $("#footer-note").text("Copyright © "+ currentYear +" Eassave. All Rights Reserved.");
+  });
+</script>
+
+
+
+<script>
+
+
+
+
+
+  document.getElementById("wf-form-Login-Form").addEventListener("submit", async function(e) {
+    e.preventDefault();
+    alert("sample");
+    const formData = new FormData(this);
+
+    const response = await fetch("../../private/auth/login-code.php", {
+      method: "POST",
+      body: formData
+    });
+
+
+
+  // Control visibility based on result status
+    if (result.message === "success") {
+
+     // Redirect to login.php after success
+      window.location.href = ".../../auth/otp.php";
+
+      alert("result.message");
+      this.reset();
+    } else {
+      alert(result.message);
+    }
+  });
+
+
+
+
+// Get the password fields and their corresponding toggle icons
+  const togglePassword = document.getElementById('togglePassword');
+  const passwordField = document.getElementById('Password');
+  const toggleConfirmPassword = document.getElementById('toggleConfirmPassword');
+  const confirmPasswordField = document.getElementById('Confirm-Password');
+
+// Function to toggle password visibility
+  function toggleVisibility(inputField, toggleIcon) {
+    const type = inputField.type === 'password' ? 'text' : 'password';
+    inputField.type = type;
+
+  // Toggle the icon based on visibility
+    const icon = type === 'password' ? 'eye-slash.svg' : 'eye.svg';
+    toggleIcon.src = `.../../images/${icon}`;
+  }
+
+// Add event listeners to toggle icons
+  togglePassword.addEventListener('click', function () {
+    toggleVisibility(passwordField, togglePassword);
+  });
+
  
 
-<!-- Footer section -->
-<?php include '../partials/footer-dark.html'; ?>
 
-    </div>
-  </div>
-  <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=665f147b743ba95cae446cfe" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-  <script src=".../../js/webflow.js" type="text/javascript"></script>
-  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-  <script>
-// Get the current year
-var currentYear = new Date().getFullYear();
-// Get the current year when the page loads
-$(document).ready(function() {
-  $("#footer-note").text("Copyright © "+ currentYear +" Eassave. All Rights Reserved.");
-});
 </script>
+
+
+
 </body>
 </html>
